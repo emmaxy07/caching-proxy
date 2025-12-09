9574214f3cb74321bca6fddf49b363bfc6b09b50
Emmanuel Omole <emmanueliyare7@gmail.com>
2025-12-08 21:17:30 -0500

    code setup

---
+++ CachingProxy.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CachingProxy {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String url = "";
+        String userInput = "";
+        String portNumber = "";
+
+        while(true){
+            System.out.print("What is your url? ");
+            url = scanner.nextLine();
+            System.out.print("");
+            if(userInput.equals("caching-proxy --port" + portNumber + "--origin" + url )){
+                
+            }
+            
+
+
+        }
+    }
+}
